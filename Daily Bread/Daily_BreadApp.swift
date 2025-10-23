2c68bd80f7f116c95ee5db429d82b017bfa27ed9
mrjuliusmars <hodgepodge2017@gmail.com>
2025-10-23 11:01:54 -0300

    Initial Commit

---
+++ Daily Bread/Daily_BreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_BreadApp.swift
+//  Daily Bread
+//
+//  Created by Marshall Hodge on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Daily_BreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Daily BreadTests/Daily_BreadTests.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_BreadTests.swift
+//  Daily BreadTests
+//
+//  Created by Marshall Hodge on 10/23/25.
+//
+
+import Testing
+@testable import Daily_Bread
+
+struct Daily_BreadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
